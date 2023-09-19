2c717f32fd47614f3f37305744452305ef33e91c
Aleir Sanches <aleir.sanches@hotmail.com>
2023-09-19 18:03:21 -0300

    testando metodo array.slice()

---
+++ dividindoSala.js
@@ -0,0 +1,27 @@
+const turma= ["Aleir",
+"Corol",
+"João",
+"Marina",
+"Neia",
+"Celio",
+"Olci",
+"Nesia",
+"Jairo",
+"Wagner",
+"Patricia",
+"Guilherme.B",
+"Jacqueline",
+"Nilson",
+"Guimlherm.S",
+"Gustavo",
+"Rafaela",
+"Alceu",
+
+
+];
+
+
+const sala1 =turma.slice(0, turma.length/2);
+console.log(sala1);
+const sala2 = turma.slice(turma.length/2);
+console.log(sala2);
