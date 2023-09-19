418bd7c4944c51d3debc2d6697b9c1ceda416b2c
Aleir Sanches <aleir.sanches@hotmail.com>
2023-09-19 18:16:52 -0300

    usando metodo array.splice()

---
+++ metodoSplice.js
@@ -0,0 +1,12 @@
+const alunos= ["Aleir",
+"Corol",
+"João",
+"Marina",
+"Neia",
+"Celio",];
+console.log(alunos);
+alunos.splice(2,2, "Sophia"); // remove o João que esta no inicice 2 e como o segundo paremetro e 2 removeo 2 valores no caso Marina, 
+//e  adiciona Sophia no lugar
+
+console.log(alunos);
+
