f7727900b93472025f1c7c41d41a65ce27c87d90
Aleir Sanches <aleir.sanches@hotmail.com>
2023-09-21 19:54:19 -0300

    listas de duas dimensões

---
+++ dimensaoesArray.js
@@ -0,0 +1,17 @@
+const alunos = ["Pedro", "Gisela", "Vinicius", "Erika"];
+const notas = [8,10,7.5,9.8];
+
+const turma = [alunos, notas];
+
+for( let i =0; i< alunos.length; i++){
+    
+
+        console.log(alunos[i]);
+        console.log(notas[i]);
+   
+};
+
+
+
+
+   
