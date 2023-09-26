73e1a843cc7ceeb2ffd57236fbf9c0ecbdf5ff66
Aleir Sanches <aleir.sanches@hotmail.com>
2023-09-25 23:30:49 -0300

    criando nova lista

--- buscandoAluno.js
+++ buscandoAluno.js
@@ -1,18 +1,21 @@
-const alunos = ["Pedro", "Gisela", "Vinicius", "Erika"];
+const estudantes = ["Pedro", "Gisela", "Vinicius", "Erika"];
 const notas = [8,10,7.5,9.8];
 
-const turma = [alunos, notas];
+const turma = [estudantes, notas];
 
 function buscandoAlunoMedia(aluno){
     if (turma[0].includes(aluno)){
-     const indice = turma[0].indexOf(aluno);
-     const media = turma[1][indice];
-     console.log( "O aluno "+ aluno + ' tem a média ' + media)
+     //const alunos = turma[0];
+     //const media = turma[1];
+     const [alunos, media] = turma; // esta forma funciano igual as de cima
+     const indice = alunos.indexOf(aluno);
+     
+     const mediaAluno = media[indice];
+     console.log( "O aluno "+ aluno + ' tem a média ' + mediaAluno)
     }
     else {
         console.log(" Aluno não encontrado")
     }
 }
 
-buscandoAlunoMedia("Gisela")
-
+buscandoAlunoMedia("Vinicius")
