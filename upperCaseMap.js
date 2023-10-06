8d6fd60ae8cf868664b591cf2e33ab65ed3ef0cd
Aleir Sanches <aleir.sanches@hotmail.com>
2023-10-05 22:33:14 -0300

    usando toUpperCase no map

---
+++ upperCaseMap.js
@@ -0,0 +1,6 @@
+const nomes = ["maria Aparecida", "João pedro", "MARINA SANCHES", "carol peixoto"];
+
+const nomesPadronizados= nomes.map((nome) => nome.toUpperCase());
+
+console.log(nomesPadronizados);
+
